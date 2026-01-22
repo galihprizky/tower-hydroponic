6f176dbe733075bf9550843e574773fa86240253
Galih <galieprakoso@gmail.com>
2026-01-23 00:09:11 +0700

    data

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   user: "postgres",        // ganti sesuai user Anda
   host: "localhost",
   database: "postgres",
-  password: "q1w2e3r4t5", // ganti
+  password: "galie0921", // ganti
   port: 5432,
 });
 
